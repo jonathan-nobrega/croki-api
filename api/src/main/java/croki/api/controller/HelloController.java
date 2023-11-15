443b6c8fb63bbf83e29385aff99e0a0bc33f4704
Jonathan Nobrega <eng.jonathan.moraes@gmail.com>
2023-11-15 10:03:23 -0500

    changes test

--- api/src/main/java/croki/api/controller/HelloController.java
+++ api/src/main/java/croki/api/controller/HelloController.java
@@ -2,6 +2,7 @@ package croki.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,7 @@ public class HelloController {
 
     @GetMapping
     public String helloWorld() {
-        return "server pinged!";
+        return "server pinged! update";
     }
+
 }
